035f1c007353ce7c53cdf0d55be2524842915adb
Antti Leinonen <antti.leinonen@helsinki.fi>
2024-08-22 10:05:36 +0300

    Remove prefix-paths

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,6 @@ const blue = require("@material-ui/core/colors/red").default
 const CourseSettings = require('./course-settings')
 
 module.exports = {
-  pathPrefix: "/ohjelmointi-24-sv",
   siteMetadata: {
     title: CourseSettings.name,
     siteUrl: CourseSettings.siteUrl,
